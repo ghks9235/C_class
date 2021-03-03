4867bd05263291f618c9b731be2bde77ab8381a9
ghks9235 <79901717+ghks9235@users.noreply.github.com>
2021-03-03 17:32:08 +0900

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,150 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	int a, b;
+	int c=30, d=40;
+	
+	a=10;
+	b=20;
+	
+	printf("%d%d \n", a, b);
+	printf("%d%d \n", c, d);
+	
+	return 0;
+
+	
+	int a, b;
+	a=10;
+	b=20;
+	int result;
+	
+
+	a=10; //a변수는 10입니다.
+	b=20; //b변수는 20입니다. 
+	
+	result = a + b;
+	
+	printf("a=%d\nb=%d\n", a, b);
+	printf("두 수의 덧셈 결과는 result = %d + %d = %d\n", a, b, result);
+	
+	return 0;
+	
+	
+	int a=1, b=10, result=0;
+	printf("a=%d\nb=%d\n",a,b);
+	while(a<=b)
+{result = result+a;
+		a=a+1;
+}
+		printf("result=%d\n",result);
+		
+	 return 0;
+	 
+	 
+	 int =a; // t: 변수의 a의 초기값 보관용
+	  
+	 int res1=0, res2=0; //res1: 홀수 합 res2: 짝수  합 
+	 for(int i=a, i<=b; i++)
+	 {
+	 	 if(i%2==0) // 짝수
+	 	 {
+	 	 	res2 +=i; 
+		  }
+		  else // 홀수
+		  {
+		  	res1 += i; 
+		   } 
+		if(i%5==0)  // 5의 배수
+		{
+			printf("%7d",i);
+		 } 
+		 if(i%9==0) res9+=i;
+		 else if(i%8==0) res8+=i;
+		 else if(i%8==0) res7+=i;
+		 else if(i%8==0) res6+=i;
+		 else if(i%8==0) res5+=i;
+		 else if(i%8==0) res4+=i;
+		 else if(i%8==0) res3+=i;
+		 else if(i%8==0) res2+=i;
+		 else res1 += i;
+		 
+	 }
+	 printf("반복문을 이용한 누적계산\n");
+	 printf("a = %d\nb=%d\n", t, b);
+	 printf("짝수의 합 =%d\n", res2);
+	 printf("홀수의 합 = %d\n", res1);
+	 
+	 */
+	 
+	 int Num;
+	 
+	 printf("숫자를 입력하시오:\n");
+	 scanf("%d",&Num);
+	 
+	 switch(Num)
+	 {
+	 	case 1:
+	 		printf("One\n");
+	 		break;
+	 		
+	 	case 2:
+	 		printf("Two\n");
+	 		break;
+	
+	 	case 3:
+	 		printf("Three\n");
+	 		break;
+			 
+	 	case 4:
+	 		printf("Four\n");
+	 		break;
+			 
+	 	case 5:
+	 		printf("Five\n");
+	 		break;
+			 
+	 	case 6:
+	 		printf("Six\n");
+	 		break;
+			 
+	 	case 7:
+	 		printf("Seven\n");
+	 		break;
+			 
+	 	case 8:
+	 		printf("Eight\n");
+	 		break;
+			 
+	 	case 9:
+	 		printf("Nine\n");
+	 		break;
+			 
+	 	case 0:
+	 		printf("Zero\n");
+			 break;		 	
+	 }
+	 return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
